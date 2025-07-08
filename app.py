20e2845aa0259618201ad11a216ec2f442986812
Christopher Johnstone <chrisj97@proton.com>
2025-07-08 21:18:40 +0100

    Added profitability query (unfinished)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser, Namespace
 from os import environ
 from pathlib import Path
 from typing import Any
@@ -14,18 +15,19 @@ SHOP_URL: str = f"{MERCHANT}.myshopify.com"
 API_VERSION: str = "2024-07"
 
 
-def get_results() -> None:
-    query_path: Path = QUERY_DIR / "orders.graphql"
+def get_results(query_path: Path) -> None:
     query: str = query_path.read_text()
     result: JSONObject = json.loads(GraphQL().execute(query=query))
-    for value in result["data"]["orders"]["edges"]:
-        print(value["node"])
+    print(json.dumps(result, indent=2))
 
 
-def main() -> None:
+def main(query_path: Path) -> None:
     with Session.temp(SHOP_URL, API_VERSION, environ["TOKEN"]):
-        get_results()
+        get_results(query_path)
 
 
 if __name__ == "__main__":
-    main()
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument("query_path", nargs=1, type=Path)
+    args: Namespace = parser.parse_args()
+    main(args.query_path[0])
