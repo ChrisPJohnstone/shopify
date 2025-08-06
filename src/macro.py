2024f35609275902768b1dda8ff852a0abd9d2db
Christopher Johnstone <chrisj97@proton.com>
2025-08-06 18:30:29 +0100

    Moved librecalc functions to class

--- src/macro.py
+++ src/macro.py
@@ -16,33 +16,40 @@ INVENTORY_ITEMS_SHEET: str = "products"
 ORDERS_SHEET: str = "orders"
 
 
-def get_sheet(sheets: Any, name: str) -> Any:
-    present: bool = sheets.hasByName(name)
-    if not present:
-        raise ValueError(f"Sheet {name} does not exist")
-    return sheets.getByName(name)
+class LibreCalc:
+    """
+    Type hints are pretty meaningless because of poor support from library
+    """
 
+    def __init__(self) -> None:
+        self.doc: Any = XSCRIPTCONTEXT.getDocument()  # type: ignore
+        self.sheets: Any = self.doc.getSheets()
 
-def write_sheet(sheet: Any, data: Iterable[Iterable[Any]]) -> None:
-    # TODO: Delete all data before writing data
-    for y, row in enumerate(data):
-        for x, value in enumerate(row):
-            cell = sheet.getCellByPosition(x, y)
-            cell.String = value
+    def get_sheet(self, name: str) -> Any:
+        present: bool = self.sheets.hasByName(name)
+        if not present:
+            raise ValueError(f"Sheet {name} does not exist")
+        return self.sheets.getByName(name)
+
+    def write_sheet(self, sheet: Any, data: Iterable[Iterable[Any]]) -> None:
+        # TODO: Delete all data before writing data
+        for y, row in enumerate(data):
+            for x, value in enumerate(row):
+                cell = sheet.getCellByPosition(x, y)
+                cell.String = value
 
 
 def main() -> None:
     controller: Controller = Controller(PROJECT_DIR)
     controller.update_inventory_items()
     controller.update_orders()
-    doc = XSCRIPTCONTEXT.getDocument()  # type: ignore
-    sheets = doc.getSheets()
-    write_sheet(
-        sheet=get_sheet(sheets, INVENTORY_ITEMS_SHEET),
+    spreadsheet_client: LibreCalc = LibreCalc()
+    spreadsheet_client.write_sheet(
+        sheet=spreadsheet_client.get_sheet(INVENTORY_ITEMS_SHEET),
         data=controller.get_inventory_items(),
     )
-    write_sheet(
-        sheet=get_sheet(sheets, ORDERS_SHEET),
+    spreadsheet_client.write_sheet(
+        sheet=spreadsheet_client.get_sheet(ORDERS_SHEET),
         data=controller.get_orders(),
     )
     return
